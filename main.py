a726713efb772b0aced0d541b689b66f3187d771
chanukya43 <crg9968@nyu.edu>
2022-11-20 15:25:49 -0500

    Fix bug in preprocess for vocabulary index

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from train import *
 import argparse
 import os
 import numpy as np
+import pickle
 import torch
 import torch.nn as nn
 import torchvision.transforms as transforms
@@ -17,21 +18,22 @@ torch.manual_seed(random_seed)
 
 parser = argparse.ArgumentParser(description='VQA')
 
-def get_model():
-    return VQABaseline()
+def get_model(vocab_size):
+    return VQABaseline(vocab_size = vocab_size)
 
-device    = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-data_dir  = "/scratch/crg9968/datasets"
-model_dir = "/scratch/crg9968/checkpoints"
-t = transforms.Compose([
-            # transforms.Scale((256, 256)),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            transforms.Normalize(
-                mean=[0.485, 0.456, 0.406],
-                std=[0.229, 0.224, 0.225])])
-train_ds = VQADataset(data_dir, top_k = 1000, transform = t)
-val_ds   = VQADataset(data_dir, mode = 'val', top_k = 1000, transform = t)
+device     = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+data_dir   = "/scratch/crg9968/datasets"
+model_dir  = "/scratch/crg9968/checkpoints"
+
+vocab_size = len(pickle.load(open(os.path.join(data_dir, 'questions_vocab.pkl'), 'rb'))["word2idx"])
+
+transform  = transforms.Compose([
+                 # transforms.Scale((256, 256)),
+                 transforms.CenterCrop(224),
+                 transforms.ToTensor(),
+                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+train_ds  = VQADataset(data_dir, top_k = 1000, transform = transform)
+val_ds    = VQADataset(data_dir, mode = 'val', top_k = 1000, transform = transform)
 
 num_gpus     = torch.cuda.device_count()
 batch_size   = 64 * num_gpus # Same batch size on each GPU
@@ -43,8 +45,8 @@ momentum     = 0.9
 weight_decay = 0.0005
 epochs       = 2
 
-model 		 = nn.DataParallel(get_model()).to(device)
-optimizer    = SGD(model.parameters(), lr=lr, momentum=momentum, weight_decay=weight_decay)
+model        = nn.DataParallel(get_model(vocab_size)).to(device) if num_gpus > 1 else get_model(vocab_size).to(device)
+optimizer    = SGD(model.parameters(), lr = lr, momentum = momentum, weight_decay = weight_decay)
 loss_fn      = nn.CrossEntropyLoss()
 scheduler    = lr_scheduler.MultiStepLR(optimizer, milestones = [60, 120, 180], gamma = 0.1)
 
